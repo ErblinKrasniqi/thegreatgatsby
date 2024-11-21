24e5ab6d927106abde905fa9fa520864880fa2f9
Erblin Krasniqi <erblini.kr@gmail.com>
2024-11-21 20:30:30 +0100

    fixed issues

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,5 @@
 "use client";
 import React, { useState } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 import Navbar from "./shared/Navbar/Navbar";
 // Import your components for the other pages (commented for now)
